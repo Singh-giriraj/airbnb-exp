7196d6844094519bbf6b5b98c93c12389bcc80e4
Singh-giriraj <96460336+Singh-giriraj@users.noreply.github.com>
2022-03-09 01:02:34 +0530

    skip

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import group from "../images/group.png";
+
+function Hero() {
+  return (
+    <section>
+      <img src={group} />
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
+export default Hero;
